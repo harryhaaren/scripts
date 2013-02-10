0b3e026225f8bca49861613e8b01a37f1e2d6132
Harry van Haaren <harryhaaren@gmail.com>
2013-02-10 23:56:52 +0000

    -Added catExternalIp.sh

---
+++ network/catExternalIp.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+
+echo -e "\e[34m:: \e[97mConnecting..."
+IP=`wget -q -O - checkip.dyndns.org | sed -e 's/[^[:digit:]\|.]//g'`
+echo -e "\e[1A\e[73C \e[34m[\e[97mDONE\e[34m]"
+
+
+echo -e "\033[1A\e[34m:: \e[97mExternal IP address:  $IP \e[97m "
+
