f497c7e76c3b41b488fd28975c91b8d1ba47b23f
Harry van Haaren <harryhaaren@gmail.com>
2013-02-11 00:09:13 +0000

    -Updated client ip details for shareWifiToEthernet, and cleaned up catExternalIp.sh

--- network/shareWifiToEthernet.sh
+++ network/shareWifiToEthernet.sh
@@ -11,8 +11,12 @@ echo -e "\e[1A\e[73C \e[34m[\e[97mDONE\e[34m]"
 
 echo -e "\e[34m:: \e[97mIP forwarding..."
 echo "1" > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o wlan0 -s 192.168.7.0/24 -j MASQUERADE
 echo -e "\e[1A\e[73C \e[34m[\e[97mDONE\e[34m]"
 
-iptables -t nat -A POSTROUTING -o wlan0 -s 192.168.7.0/24 -j MASQUERADE
-
+echo -e "\e[34m:: \e[97mEg IP config for client:
+   Static IP: 192.168.2.2
+   Gateway  : 192.168.2.1
+   DNS      : 192.168.2.1"
+echo -e "\e[1A\e[73C \e[34m[\e[97mDONE\e[34m]"
 echo -e "\e[97m"
