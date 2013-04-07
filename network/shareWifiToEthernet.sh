1f545674637f98dd2accf41c0c9e8a2dfc6095a3
Harry van Haaren <harryhaaren@gmail.com>
2013-04-07 20:58:36 +0100

    -Updated shareWifi to work, and provide DNS using /etc/resolv.conf

--- network/shareWifiToEthernet.sh
+++ network/shareWifiToEthernet.sh
@@ -6,17 +6,21 @@ ifconfig eth0 up
 echo -e "\e[1A\e[73C \e[34m[\e[97mDONE\e[34m]"
 
 echo -e "\e[34m:: \e[97mInterface IP..."
-ifconfig eth0 192.168.7.1
+ifconfig eth0 192.168.2.1
 echo -e "\e[1A\e[73C \e[34m[\e[97mDONE\e[34m]"
 
 echo -e "\e[34m:: \e[97mIP forwarding..."
 echo "1" > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING -o wlan0 -s 192.168.7.0/24 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o wlan0 -s 192.168.2.0/24 -j MASQUERADE
 echo -e "\e[1A\e[73C \e[34m[\e[97mDONE\e[34m]"
+
+# get current DNS server from /etc/resolv.conf
+DNS=`sed -n '4p' /etc/resolv.conf`
+DNSIP="${DNS:11}"
 
 echo -e "\e[34m:: \e[97mEg IP config for client:
    Static IP: 192.168.2.2
    Gateway  : 192.168.2.1
-   DNS      : 192.168.2.1"
+   DNS      : $DNSIP"
 echo -e "\e[1A\e[73C \e[34m[\e[97mDONE\e[34m]"
 echo -e "\e[97m"
